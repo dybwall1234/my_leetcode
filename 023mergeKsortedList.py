c267ed2fc38199fdcf8a14b604c0968491fb73aa
dybwall1234 <244198337@qq.com>
2019-01-22 16:59:43 +0800

    new

---
+++ 023mergeKsortedList.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+#
+# Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+#
+"""
+This module provide ....
+
+Authors: duanyibo(duanyibo@baidu.com)
+Date:    2019/1/22 下午3:33
+"""
+from Queue import PriorityQueue
+
+from LinkedListCycle import ListNode, LinkedList
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
+
+    def divideNconquer(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else lists
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next = l2
+        else:
+            point.next = l1
+        return head.next
+
+data = [[1,4,5],[1,3,4],[2,6]]
+listLink = []
+for list in data:
+    linkList = LinkedList()
+    listLink.append(linkList.initList(list))
+
+s = Solution()
+re = s.divideNconquer(listLink)
+re.printList
